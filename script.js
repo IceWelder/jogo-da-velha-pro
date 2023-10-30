be27d509bd9218fb3cd9a0b3865cafa8001d1466
Pedro Augusto <102700254+IceWelder@users.noreply.github.com>
2023-10-30 12:32:59 -0300

    game2

--- script.js
+++ script.js
@@ -5,57 +5,72 @@ let gameEnded = false;
 // Função chamada quando um quadrado é clicado
 function play(square) {
     if (gameEnded) {
-      return;
+        return;
     }
-  
+
     if (square.innerText === '') {
-      square.innerText = currentPlayer;
-      if (checkWin()) {
-        // Alguém ganhou
-        alert(`Jogador ${currentPlayer} venceu!`);
-        gameEnded = true;
-        // Adicione a classe de vencedor ao elemento vencedor
-        square.classList.add(`winner-${currentPlayer}`);
-      } else if (checkDraw()) {
-        // Empate
-        alert('Empate! O jogo terminou sem vencedor.');
-        gameEnded = true;
-        // Adicione a classe de empate a todos os elementos
-        const squares = document.querySelectorAll('.square');
-        squares.forEach(square => square.classList.add('draw'));
-      } else {
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-      }
+        square.innerText = currentPlayer;
+        if (checkWin()) {
+            // Alguém ganhou
+            alert(`Jogador ${currentPlayer} venceu!`);
+            gameEnded = true;
+            // Adicione a classe de vencedor ao elemento vencedor
+            square.classList.add(`winner-${currentPlayer}`);
+        } else if (checkDraw()) {
+            // Empate
+            alert('Empate! O jogo terminou sem vencedor.');
+            gameEnded = true;
+            // Adicione a classe de empate a todos os elementos
+            const squares = document.querySelectorAll('.square');
+            squares.forEach(square => square.classList.add('draw'));
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
     }
-  }
-  
+}
 
 // Função para verificar se alguém venceu
 function checkWin() {
-  const squares = document.querySelectorAll('.square');
-  const winningCombos = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
-    [0, 4, 8], [2, 4, 6] // Diagonais
-  ];
+    const squares = document.querySelectorAll('.square');
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
+        [0, 4, 8], [2, 4, 6] // Diagonais
+    ];
 
-  for (const combo of winningCombos) {
-    const [a, b, c] = combo;
-    if (squares[a].innerText && squares[a].innerText === squares[b].innerText && squares[a].innerText === squares[c].innerText) {
-      return true; // Temos um vencedor
+    for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (squares[a].innerText && squares[a].innerText === squares[b].innerText && squares[a].innerText === squares[c].innerText) {
+            return true; // Temos um vencedor
+        }
     }
-  }
 
-  return false; // Ninguém venceu ainda
+    return false; // Ninguém venceu ainda
 }
 
 // Função para verificar se há um empate
 function checkDraw() {
-  const squares = document.querySelectorAll('.square');
-  for (const square of squares) {
-    if (square.innerText === '') {
-      return false; // Ainda há espaços vazios, o jogo não terminou em empate
+    const squares = document.querySelectorAll('.square');
+    for (const square of squares) {
+        if (square.innerText === '') {
+            return false; // Ainda há espaços vazios, o jogo não terminou em empate
+        }
     }
-  }
-  return true; // Todos os espaços estão preenchidos, é um empate
+    return true; // Todos os espaços estão preenchidos, é um empate
+}
+
+function restartGame() {
+  // Limpa o conteúdo das células do tabuleiro
+  const cells = document.querySelectorAll('.square');
+  cells.forEach(cell => {
+      cell.innerHTML = '';
+      cell.classList.remove('winner-X', 'winner-O', 'draw');
+  });
+
+  // Reinicia o jogador para 'X'
+  currentPlayer = 'X';
+
+  // Limpa a mensagem
+  const message = document.getElementById('message');
+  message.textContent = '';
 }
